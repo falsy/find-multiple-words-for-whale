b5240f8a91856fd8f4db4a7a75203e64c49db32e
falsy <me@falsy.me>
2019-03-30 19:26:14 +0900

    모든 BODY 재귀

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-let defaultElement = '';
+let bodyHTML = '';
+const exceptEl = ['SCRIPT', 'LINK', 'STYLE', 'IFRAME'];
 
 whale.tabs.executeScript({
   code: 'document.body.innerHTML'
 }, (html) => {
-  defaultElement = html;
-  console.log(defaultElement[0]);
-
   const el = document.createElement('body');
   el.innerHTML = html;
-  console.log(el);
+  findAllDom(el, modifyDom);
 });
 
+function modifyDom(target) {
+  console.log(target);
+}
 
-// console.log(document.getElementById('keyword'));
+function findAllDom(target, modifyDomEl) {
+  if(target.length) {
+    for(let i=0, len=target.length; i<len; i++) {
+      findAllDom(target[i], modifyDomEl);
+    }
+  } else {
+    modifyDomEl(target);
+    if(target.children && target.children.length) {
+      for(let i=0, len=target.children.length; i<len; i++) {
+        if(exceptEl.indexOf(target.children[i].tagName) === -1) {
+          findAllDom(target.children[i], modifyDomEl);
+        }
+      }
+    }
+  }
+}
 
-setTimeout(() => {
-  whale.tabs.executeScript({
-    code: 'document.getElementById("page").style.backgroundColor="red"'
-  });
-}, 2000);
+// setTimeout(() => {
+//   whale.tabs.executeScript({
+//     code: 'document.getElementById("page").style.backgroundColor="red"'
+//   });
+// }, 2000);
 
-setTimeout(() => {
-  whale.tabs.executeScript({
-    code: 'document.body.innerHTML=`'+defaultElement+'`'
-  });
-}, 4000);
+// setTimeout(() => {
+//   whale.tabs.executeScript({
+//     code: 'document.body.innerHTML=`'+defaultElement+'`'
+//   });
+// }, 4000);
