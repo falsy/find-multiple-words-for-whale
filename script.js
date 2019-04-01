394a8cff93ad398b5e5d40dd8541824ba5a2cb4f
falsy <me@falsy.me>
2019-04-01 21:28:30 +0900

    문장 형태의 텍스트 노드를 나누는 개발중

--- script.js
+++ script.js
@@ -15,27 +15,53 @@ inputEl.addEventListener('keydown', (e) => {
 
 const findAllDom = function() {
   class actionFindAllDom {
-    constructor() {
+    constructor(keywords) {
       this.body = document.body;
       this.exceptEl = ['SCRIPT', 'LINK', 'STYLE', 'IFRAME'];
-      this.findWord = 'About';
+      this.findWord = keywords;
     }
 
     insertFmwElement(el) {
       el.childNodes.forEach(node => {
-        if(node.nodeName === '#text') {
-          if(node.data.indexOf(this.findWord) !== -1) {
-            const fmwElement = document.createElement('i');
-            fmwElement.className = 'fmw-style';
-            fmwElement.innerText = this.findWord;
-            node.parentNode.replaceChild(fmwElement, node);
+        if(node.nodeName === '#text' && this.exceptEl.indexOf(node.parentNode.nodeName) === -1) {
+          const nodeSpaceArr = node.data.split(' ');
+          const checkTrueData = nodeSpaceArr.filter(data => {
+            data = data.replace(/\t|\n| /g, '');
+            return data !== "";
+          });
+
+          if(checkTrueData.length > 1) {
+            // console.log(node.data);
+            // // console.dir(node);
+            // let currntNode = node;
+            // checkspace.forEach((text) => {
+            //   // console.dir(text);
+            //   currntNode.splitText(text.length);
+            //   // console.dir(currntNode);
+            //   // this.replaceElement(currntNode);
+            //   currntNode = node.nextSibling;
+            // });
+          } else {
+            // console.log('end');
+            if(node.data.replace(/\t|\n| /g, '') !== "") console.log(node);
+            // this.replaceElement(node);
           }
+
         } else {
           if(node.childNodes && node.childNodes.length && this.exceptEl.indexOf(node.nodeName) === -1) {
             this.insertFmwElement(node);
           }
         }
       });
+    }
+
+    replaceElement(node) {
+      if(node.data.indexOf(this.findWord) !== -1) {
+        const fmwElement = document.createElement('i');
+        fmwElement.className = 'fmw-style';
+        fmwElement.innerText = this.findWord;
+        node.parentNode.replaceChild(fmwElement, node);
+      }
     }
 
     deleteFmwElement(el) {
@@ -60,7 +86,7 @@ const findAllDom = function() {
 
 whale.tabs.executeScript({
   code: `
-  window.fmwClass = new ${findAllDom()}();
+  window.fmwClass = new ${findAllDom()}('About');
   `
 });
 
