1db17dc2f36b10d9a009b6eeaafe38d546a7803e
falsy <me@falsy.me>
2019-04-03 20:52:32 +0900

    여러개의 키워드 검색 기능 / 스크롤에 위치 마크 추가

--- script.js
+++ script.js
@@ -1,52 +1,17 @@
-const inputEl = document.getElementById('keyword');
-let keywords = [];
-
-inputEl.addEventListener('keydown', (e) => {
-  setTimeout(() => {
-    if(e.keyCode === 13 && inputEl.value) {
-      keywords = inputEl.value.split(',');
-      keywords.forEach((keyword, i) => {
-        keywords[i] = keyword.trim();
-      });
-    }
-  });
-});
-
-
 const findAllDom = function() {
   class actionFindAllDom {
-    constructor(keywords) {
+    constructor() {
       this.body = document.body;
       this.exceptEl = ['SCRIPT', 'LINK', 'STYLE', 'IFRAME'];
-      this.findWord = keywords;
     }
 
     insertFmwElement(el) {
       el.childNodes.forEach(node => {
-        if(node.nodeName === '#text' && this.exceptEl.indexOf(node.parentNode.nodeName) === -1) {
-          const nodeSpaceArr = node.data.split(' ');
-          const checkTrueData = nodeSpaceArr.filter(data => {
-            data = data.replace(/\t|\n| /g, '');
-            return data !== "";
-          });
-
-          if(checkTrueData.length > 1) {
-            // console.log(node.data);
-            // // console.dir(node);
-            // let currntNode = node;
-            // checkspace.forEach((text) => {
-            //   // console.dir(text);
-            //   currntNode.splitText(text.length);
-            //   // console.dir(currntNode);
-            //   // this.replaceElement(currntNode);
-            //   currntNode = node.nextSibling;
-            // });
-          } else {
-            // console.log('end');
-            if(node.data.replace(/\t|\n| /g, '') !== "") console.log(node);
-            // this.replaceElement(node);
+        if(node.nodeName === '#text' && this.exceptEl.indexOf(node.parentNode.nodeName) === -1
+          && node.parentNode.className !== 'fmw-style-container') {
+          if(node.data.replace(/\t|\n| /g, '') !== "") {
+            this.replaceElement(node);
           }
-
         } else {
           if(node.childNodes && node.childNodes.length && this.exceptEl.indexOf(node.nodeName) === -1) {
             this.insertFmwElement(node);
@@ -56,18 +21,42 @@ const findAllDom = function() {
     }
 
     replaceElement(node) {
-      if(node.data.indexOf(this.findWord) !== -1) {
-        const fmwElement = document.createElement('i');
-        fmwElement.className = 'fmw-style';
-        fmwElement.innerText = this.findWord;
+      const posMark = this.elementAbsPositionTop(node.parentNode);
+      const fmwElement = document.createElement('i');
+            fmwElement.className = 'fmw-style-container';
+      let nodeText = node.data;
+
+      this.findWords.forEach((word, i) => {
+        if(nodeText.indexOf(word) !== -1) {
+          const reg = new RegExp(word, 'gm');
+          nodeText = nodeText.replace(reg, `
+            <i class="fmw-style fmw-style-${i}">${word}</i>
+          `);
+          this.scrollMarkElement.innerHTML += `
+            <i class="fmw-style-scroll-mark fmw-style-${i}" style="top: ${posMark}px;"><i>
+          `;
+        }
+      });
+
+      if(node.data !== nodeText) {
+        fmwElement.innerHTML = nodeText;
         node.parentNode.replaceChild(fmwElement, node);
       }
     }
 
+    elementAbsPositionTop(el) {
+      let posTop = 0;
+      while(el.offsetParent) {
+        posTop += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+      }
+      return posTop;
+    }
+
     deleteFmwElement(el) {
       for(const node of el.children) {
-        if(String(node.className).indexOf('fmw-style') !== -1 && node.nodeName === 'I') {
-          node.outerHTML = this.findWord;
+        if(String(node.className).indexOf('fmw-style-container') !== -1 && node.nodeName === 'I') {
+          node.outerHTML = node.textContent;
         } else {
           if(node.children && node.children.length && this.exceptEl.indexOf(node.nodeName) === -1) {
             this.deleteFmwElement(node);
@@ -76,9 +65,72 @@ const findAllDom = function() {
       }
     }
 
-    searchDomElement(isInsert = false) {
+    searchDomElement(keywords) {
+      this.scrollMarkElement = document.getElementById('fwm-scroll');
+      this.scrollMarkElement.innerHTML = '';
       this.deleteFmwElement(this.body);
-      if(isInsert) this.body.childNodes.forEach(node => this.insertFmwElement(node));
+      if(keywords.length) {
+        this.findWords = keywords;
+        this.body.childNodes.forEach(node => this.insertFmwElement(node));
+      }
+    }
+
+    prependStyleSheet() {
+      if(document.getElementById('fwm-css')) return;
+      const fmwStyleElement = document.createElement('style');
+            fmwStyleElement.id = 'fwm-css';
+            fmwStyleElement.innerHTML = `
+              #fwm-scroll {
+                position: absolute;
+                top: 0;
+                right: 0;
+              }
+              .fmw-style-container {
+                font-style: normal;
+              }
+              .fmw-style-scroll-mark {
+                position: absolute;
+                width: 15px;
+                height: 2px;
+                background: inherit;
+                right: 0;
+              }
+              .fmw-style-container .fmw-style {
+                font-style: normal;
+                display: inline-block;
+                box-shadow: 1px 3px 3px rgba(0,0,0,0.2);
+                border-radius: 4px;
+                padding: 0 5px;
+              }
+              .fmw-style-0 {
+                background: red;
+                color: #fff;
+              }
+              .fmw-style-1 {
+                background: blue;
+                color: #fff;
+              }
+              .fmw-style-2 {
+                background: green;
+                color: #fff;
+              }
+              .fmw-style-3 {
+                background: yellow;
+                color: #000;
+              }
+              .fmw-style-4 {
+                background: orange;
+                color: #000;
+              }
+            `;
+      this.body.prepend(fmwStyleElement);
+    }
+
+    appendScrollMark() {
+      if(document.getElementById('fwm-scroll')) return;
+      const fmwScrollElement = document.createElement('div');
+            fmwScrollElement.id = 'fwm-scroll';
+      this.body.append(fmwScrollElement);
     }
   };
   return actionFindAllDom;
@@ -86,22 +138,24 @@ const findAllDom = function() {
 
 whale.tabs.executeScript({
   code: `
-  window.fmwClass = new ${findAllDom()}('About');
+    window.fmwClass = new ${findAllDom()}();
+    fmwClass.prependStyleSheet();
+    fmwClass.appendScrollMark();
   `
 });
 
-setTimeout(() => {
-  whale.tabs.executeScript({
-    code: `
-    fmwClass.searchDomElement(true);
-    `
-  });
-}, 2000);
-
-setTimeout(() => {
-  whale.tabs.executeScript({
-    code: `
-    fmwClass.searchDomElement(false);
-    `
-  });
-}, 4000);
+const inputEl = document.getElementById('keyword');
+inputEl.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) {
+    let keywords = inputEl.value.split(',');
+    keywords = keywords.filter(keyword => {
+      return !!keyword.trim();
+    });
+    keywords = JSON.stringify(keywords);
+    whale.tabs.executeScript({
+      code: `
+        fmwClass.searchDomElement(${keywords});
+      `
+    });
+  }
+});
